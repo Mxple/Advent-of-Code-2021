9d6582774e447eee951434bdc1be3312b193081e
RIN <ericalexli@gmail.com>
2021-12-16 07:31:19 -0500

    Update DisplayRepair.py

--- Day 8/DisplayRepair.py
+++ Day 8/DisplayRepair.py
@@ -6,17 +6,15 @@ with open(os.path.join(sys.path[0], "input.txt")) as file:
     for row in data:
         notepad[row.partition(" | ")[0]] = row.partition(" | ")[2]
 
-print (notepad)
-
 #returns true if string a is inside string b regardless of order. returns char otherwise
-def inside(str: a, str: b):
+def inside(a, b):
     for i in a:
         if i not in b:
             return i
     return True
 
 #returns true if all elements of a string are in another and vise versa. length doesn't matter.
-def same(str: a, str: b):
+def same(a, b):
     for i in a:
         if i not in b:
             return False
@@ -28,7 +26,7 @@ def same(str: a, str: b):
 def toDigits(key, puzzle):
     keys = key.split()
     puzzles = puzzle.split()
-    a,b,c,d,e,f,g,zero,one,two,three,four,five,six,seven,eight,nine,temp = ""
+    a=b=c=d=e=f=g=zero=one=two=three=four=five=six=seven=eight=nine = ""
 
     for k in keys:
         if len(k) == 2:
@@ -37,15 +35,22 @@ def toDigits(key, puzzle):
             seven = k
         if len(k) == 4:
             four == k
-        if len(k) == 8:
+        if len(k) == 7:
             eight = k
+
     a = inside(seven,one)
+
+    for k in keys:
+        if len(k) == 5:
+            if inside(one, k):
+                three = k
     for k in keys:
         if same(k,three+four):
             nine = k
 
     e = inside(eight,nine)
     g = inside(nine,four+seven)
+
     b = inside(eight, three+e+g)
     for k in keys:
         if same(e+g+b+seven,k):
@@ -58,7 +63,34 @@ def toDigits(key, puzzle):
     c = inside(eight,six)
     f = inside(one, c)
 
-    
+    enigma = {
+        a:"a",
+        b:"b",
+        c:"c",
+        d:"d",
+        e:"e",
+        f:"f",
+        g:"g"
+    }
 
-    
-    while "" in [a,b,c,d,e,f,g]:
+    decoded = []
+    for p in puzzles:
+        temp = ""
+        for char in p:
+           temp += enigma[char]
+        decoded.append(temp)
+
+
+    map = ["abcefg","cf","acdfg","bcdf","abdfg","abdfg","abdefg","acf","abcdefg","abcdfg"]
+    result = []
+    for d in range(len(decoded)):
+        for i in range(len(map)):
+            if same(puzzles[d],map[i]):
+                result.append(i)
+    return result
+
+
+display = []
+for item in notepad.items():
+    display.append(toDigits(item[0],item[1]))
+print(display)
